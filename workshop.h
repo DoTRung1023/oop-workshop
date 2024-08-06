07c343329d3a4e5bbbc956330c53f1b7f1e51939
DoTRung1023 <a1899443@adelaide.edu.au>
2024-08-06 23:21:05 +0930

    change

--- workshop.h
+++ workshop.h
@@ -9,7 +9,6 @@ void changeValue(double* number) {
 
 // part 3
 void printArray(double* number_array, int length) {
-    cout << "The numbers are: ";
     for (int i = 0; i<length; i++) {
         cout << number_array[i] << "\t";
     }
