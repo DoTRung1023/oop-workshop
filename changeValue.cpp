dd6022b4acf0c79beb8246d12168ba1868d03c39
DoTRung1023 <a1899443@adelaide.edu.au>
2024-08-12 01:37:32 +0930

    remove

--- changeValue.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-void changeValue(double* number) {
-    *number = 42;
-}
--- dynamicArray.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-double* dynamicArray(int number) {
-    double* array = new double[number]();
-
-    return array;
-}
